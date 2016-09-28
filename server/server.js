bc867c745272da2a3d12b100e36cfaed0e9ad848
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-09-28 18:00:56 -0400

    initial commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var fs      = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+
+    url = 'http://www.imdb.com/title/tt1229340/';
+
+    request(url, function (error, response, html) {
+        if(!error){
+            
+            var $ = cheerio.load(html);
+            var title, release, rating;
+            var json = {title : "", release : "", rating: ""};
+        }
+    });
+
+});
+
+app.listen('3000');
+console.log('Server port: 3000');
+
+exports = module.exports = app;
