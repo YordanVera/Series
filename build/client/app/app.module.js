5cd4b97daaa786acfcb846dab7e4b33feb96b17b
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-10-09 06:08:32 -0400

    #1 Se agrega: - Angular 2 material

--- build/client/app/app.module.js
+++ build/client/app/app.module.js
@@ -13,13 +13,14 @@ var core_1 = require('@angular/core');
 var core_2 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var app_component_1 = require('./app.component');
+var material_1 = require('@angular/material');
 core_2.enableProdMode();
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
+            imports: [platform_browser_1.BrowserModule, material_1.MaterialModule],
             declarations: [app_component_1.AppComponent],
             bootstrap: [app_component_1.AppComponent]
         }), 
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule }         from '@angular/core';
 import { enableProdMode }   from '@angular/core';
 import { BrowserModule }    from '@angular/platform-browser';
 import { AppComponent }     from './app.component';
+import { MaterialModule }   from '@angular/material';
 enableProdMode();
 
 @NgModule({
-    imports:      [ BrowserModule ],
+    imports     : [ BrowserModule, MaterialModule ],
     declarations: [ AppComponent ],
-    bootstrap:    [ AppComponent ]
+    bootstrap   : [ AppComponent ]
 })
 export class AppModule { }
