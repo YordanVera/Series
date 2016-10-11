6064c71a344509067f8a585e832e61d1e53ea7f2
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-10-11 14:44:04 -0400

    #3 se agrega: archivos de configuración para la conexión a la bd y la api de tmdb.

---
+++ source/server/config/config_tmdb.ts
@@ -0,0 +1,6 @@
+export const config_tmdb = {
+    username        : 'Mr.Kira',
+    password        : '24kira55',
+    key             : 'c77869e1ec5fe57499e7e9c25ce4c7da',
+    request_token   : null
+};
