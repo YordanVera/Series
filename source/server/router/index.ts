80a3d974f3958602235f8fa98a7db7a773155093
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-10-12 06:28:57 -0400

    #3 correcciones - el envío del id_serie, ya que es entregado por result.insertID. - el path de delete '/del_tvshow' ... se agrega '/:id_serie' como parámetro.

--- source/server/router/index.ts
+++ source/server/router/index.ts
@@ -1,10 +1,7 @@
 "use strict";
-
 import * as express from 'express';
 import * as mysql from 'mysql';
-
 import * as tvshowRoutes from './routes/tvshow';
-
 import {config_db} from '../config/config_db';
 import {config_tmdb} from '../config/config_tmdb';
 
@@ -33,22 +30,22 @@ router.post('/add_tvshow', (req, res) => {
         return res.sendStatus(400);
     }else{
         //db add
-        pool.query('INSERT INTO series (nombre) VALUES (?)', [req.body.nombre_serie], (error, result) => {
+        pool.query('INSERT INTO series (nombre) VALUES (?)', [req.body.nombre], (error, result) => {
             if (error) return res.json({success: false, error: error});
             return res.json({
                 success: true,
-                id_serie: result[0].id_serie
+                id_serie: result.insertId
             });
         });
     }
 });
 
-router.delete('/del_tvshow',(req, res) => {
-    if(!req.body){
+router.delete('/del_tvshow/:id_serie',(req, res) => {
+    if(!req.params){
         return res.sendStatus(400);
     }else{
         //db del
-        pool.query('DELETE FROM series WHERE id_serie = ?', [req.body.id_serie], (error) => {
+        pool.query('DELETE FROM series WHERE id_serie = ?', [req.params.id_serie], (error) => {
             if (error) return res.json({success: false, error: error});
             return res.json({success: true});
         });
@@ -60,7 +57,7 @@ router.put('/update_tvshow',(req, res) => {
         return res.sendStatus(400);
     }else{
         //db update
-        pool.query('UPDATE series SET nombre = ? WHERE id_serie = ?', [req.body.id_serie], (error) => {
+        pool.query('UPDATE series SET nombre = ? WHERE id_serie = ?', [req.body.nombre, req.body.id_serie], (error) => {
             if (error) return res.json({success: false, error: error});
             return res.json({success:true});
         });
