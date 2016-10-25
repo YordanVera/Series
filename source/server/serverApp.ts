870d7a42be7ada400ab1e53a916d7cfa2f3a90f1
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-10-24 21:28:51 -0400

    actualización de dependencias

--- source/server/serverApp.ts
+++ source/server/serverApp.ts
@@ -1,19 +1,28 @@
-/// <reference path="../typings/index.d.ts" />
 import * as bodyParser from "body-parser";
 import * as express from "express";
 import * as path from "path";
 import * as Router from "./router";
+import { tmdb_services } from './tmdb/tmdb_services';
 
 export class ServerApp {
-    private _app : express.Express;
+    private _app            : express.Express;
+    private tmdb_service    : any;
 
     constructor(){
         this._app = express();
         this._app.use(bodyParser.json());
         this._app.use(express.static(path.join(__dirname,'..','client')));
         this.Routes();
+        this.tmdb_service = new tmdb_services;
     }
-
+    public subTMDB(){
+        // var subject = this.tmdbService.getSubject();
+        // var subscription = subject.subscribe(
+        //     x => console.log('onNext: ' + x),
+        //     e => console.log('onError: ' + e.message),
+        //     () => console.log('onCompleted'));
+        //     this.tmdbService.getToken();
+    }
     public setRoutes(){
         //this._app.get('/*', this._indexRender);
     }
