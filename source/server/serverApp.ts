d26724e051c525fd1ee17832ef7aa81e7199e007
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-10-26 22:41:36 -0400

    #5 nueva instacia para el enrutador, se le pasa la aplicación y el servicio de la api tmdb

--- source/server/serverApp.ts
+++ source/server/serverApp.ts
@@ -1,42 +1,25 @@
 import * as bodyParser from "body-parser";
 import * as express from "express";
 import * as path from "path";
-import * as Router from "./router";
 import { tmdb_services } from './tmdb/tmdb_services';
+import { RouterETV } from './router';
 
 export class ServerApp {
     private _app            : express.Express;
     private tmdb_service    : any;
-
+    private Router          : RouterETV;
     constructor(){
         this._app = express();
         this._app.use(bodyParser.json());
         this._app.use(express.static(path.join(__dirname,'..','client')));
-        this.Routes();
         this.tmdb_service = new tmdb_services;
+        this.Router = new RouterETV(this._app, this.tmdb_service);
     }
-    public subTMDB(){
-        // var subject = this.tmdbService.getSubject();
-        // var subscription = subject.subscribe(
-        //     x => console.log('onNext: ' + x),
-        //     e => console.log('onError: ' + e.message),
-        //     () => console.log('onCompleted'));
-        //     this.tmdbService.getToken();
-    }
-    public setRoutes(){
-        //this._app.get('/*', this._indexRender);
-    }
-
-    private Routes()  {
-        this._app.use(Router);
-    }
-
     public startServer(){
         this._app.listen(5000, function(){
             console.log('running on port 5000...');
         });
     }
-
     private _indexRender(req: express.Request, res: express.Response){
         res.sendFile(path.resolve('.')+'/build/client', 'index.html');
     }
