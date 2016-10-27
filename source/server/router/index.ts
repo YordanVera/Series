0b9775f3c52fb869fe9831b7d0a3be83be6468f3
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-10-26 22:43:19 -0400

    #5 se actualiza el enrutador se mueve la lógica dentro de routes, para tener un control mas modular.

--- source/server/router/index.ts
+++ source/server/router/index.ts
@@ -1,67 +1,9 @@
-"use strict";
-import * as express from 'express';
-import * as mysql from 'mysql';
-import * as tvshowRoutes from './routes/tvshow';
-import {config_db} from '../config/config_db';
-import {config_tmdb} from '../config/config_tmdb';
-import * as path from 'path';
+import * as express from "express";
+import { tmdb_services } from '../tmdb/tmdb_services';
+import { tvshow_routes } from './routes/tvshow';
 
-let pool = mysql.createPool(config_db);
-let router = express.Router();
-
-router.get('/', (req, res) => {
-    res.sendFile(path.resolve('../build/client/').replace(/\\/g, '/'), 'index.html');
-});
-
-router.get('/list_tvshows', (req, res) => {
-    if(!req.body){
-        return res.sendStatus(400);
-    }else{
-        pool.query('SELECT id_serie, nombre FROM series', (error, rows, fields) => {
-            if (error) return res.json({success: false, error: error});
-            return res.json({
-                success: true,
-                result: rows});
-        });
+export class RouterETV {
+    constructor(app, tmdb_services){
+        new tvshow_routes(app, tmdb_services);        
     }
-});
-
-router.post('/add_tvshow', (req, res) => {
-    if(!req.body){
-        return res.sendStatus(400);
-    }else{
-        //db add
-        pool.query('INSERT INTO series (nombre) VALUES (?)', [req.body.nombre], (error, result) => {
-            if (error) return res.json({success: false, error: error});
-            return res.json({
-                success: true,
-                id_serie: result.insertId
-            });
-        });
-    }
-});
-
-router.delete('/del_tvshow/:id_serie',(req, res) => {
-    if(!req.params){
-        return res.sendStatus(400);
-    }else{
-        //db del
-        pool.query('DELETE FROM series WHERE id_serie = ?', [req.params.id_serie], (error) => {
-            if (error) return res.json({success: false, error: error});
-            return res.json({success: true});
-        });
-    }
-});
-
-router.put('/update_tvshow',(req, res) => {
-    if(!req.body){
-        return res.sendStatus(400);
-    }else{
-        //db update
-        pool.query('UPDATE series SET nombre = ? WHERE id_serie = ?', [req.body.nombre, req.body.id_serie], (error) => {
-            if (error) return res.json({success: false, error: error});
-            return res.json({success:true});
-        });
-    }
-});
-export = router;
+}
