16857295a8e3fe33e07fab955735bd74fba0ea9b
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-11-04 00:07:14 -0400

    #6 se agrega EmitterServices dentro del app component, esto para que sea el padre quien comunique a header y cover, que son sus hijos.

--- source/client/app/app.component.ts
+++ source/client/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit }    from '@angular/core';
-
+import { EmitterService }       from './emitter/emitter.service';
 @Component({
     moduleId    : module.id, 
     selector    : 'app',
-    templateUrl : './app.component.html'
+    templateUrl : './app.component.html',
+    providers:[EmitterService]
 })
 export class AppComponent{
-
+    constructor(private emitter:EmitterService){  }
 }
