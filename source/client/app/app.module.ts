1fa13ea9bbbd7c9c19587877f4aea24f4732de1f
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-11-07 16:30:55 -0400

    #6 se agrega en módulo enrutador (router) en la configuración de la app junto a la componente detail

--- source/client/app/app.module.ts
+++ source/client/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule }                 from '@angular/core';
-import { enableProdMode }           from '@angular/core';
+import { NgModule, enableProdMode } from '@angular/core';
 import { HttpModule, JsonpModule }  from '@angular/http';
 import { BrowserModule }            from '@angular/platform-browser';
 import { FormsModule }              from '@angular/forms';
 import { MaterialModule }           from '@angular/material';
+import { AppRoutingModule }         from './app-routing.module';
 import { AppComponent }             from './app.component';
 import { HeaderComponent }          from './header/header.component';
 import { BodyComponent }            from './body/body.component';
 import { CoverComponent }           from './body/cover/cover.component';
+import { DetailComponent }          from './body/detail/detail.component';
 import { newDialogComponent }       from './header/newDialog.component';
 
 enableProdMode();
@@ -17,13 +18,15 @@ enableProdMode();
         HttpModule,
         JsonpModule,
         FormsModule,
+        AppRoutingModule,
         MaterialModule.forRoot() ],
     declarations: [ 
         AppComponent,
         HeaderComponent,
         BodyComponent,
         newDialogComponent,
-        CoverComponent
+        CoverComponent,
+        DetailComponent
         ],
         entryComponents: [
             newDialogComponent
