88a204d5ecc066919cbc557e163cac5f5ac10c72
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-11-08 05:07:20 -0400

    #6 componente detail, obtiene los detalles de la serie a través del services de cover, por ahora son desplegados sin ningún estilo.

--- source/client/app/body/detail/detail.component.ts
+++ source/client/app/body/detail/detail.component.ts
@@ -1,22 +1,38 @@
-import { Component, OnInit }    from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Location }               from '@angular/common';
-
+import { Component, OnInit }        from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+import { CoverService }             from '../cover/cover.service';
+import { TVShow }                   from '../cover/tvshow';
 @Component({
     moduleId    : module.id, 
     selector    : 'detail',
-    templateUrl : './detail.component.html'
+    templateUrl : './detail.component.html',
+    providers   : [CoverService]
 })
 export class DetailComponent {
-    private title : string;
-    constructor(private route: ActivatedRoute,
-                private location: Location){}
+    private TVShow : TVShow;
+    private loading: boolean;
+    constructor(private route           : ActivatedRoute,
+                private location        : Location,
+                private coverService    : CoverService){
+                    this.TVShow = new TVShow;
+                    this.loading = true;
+                 }
     
     ngOnInit(): void {
         this.route.params.forEach((params: Params) => {
-            let data : any = params;
-            this.title = data.TVShow_name;
+            let _data : any = params;
+            this.TVShow.title = _data.TVShow_name;
+            this.TVShow.id_serie = _data.id;
         });
+        this.getDetail();
     }
-
+    getDetail(){
+        this.coverService.get_TVShow_Detail(this.TVShow.title).subscribe(
+            data => {
+                this.TVShow.data = data.result;
+                this.loading = false;
+            }
+        );
+    }
 }
