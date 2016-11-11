a48a5dd6d2992bdec78780cb1a319e2b5092008d
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-11-10 23:35:13 -0400

    #10 se agrega torrent service a la componente detalle de una episodio + se agrega una tabla para cargar los datos

--- source/client/app/body/detail/episodeDetailDialogComponent.component.ts
+++ source/client/app/body/detail/episodeDetailDialogComponent.component.ts
@@ -1,15 +1,26 @@
-import { Component, ViewChild, Input } from '@angular/core';
+import { Component, ViewChild, Input }  from '@angular/core';
+import { TorrentService }               from './torrent.service';
 @Component({
     moduleId    : module.id, 
   selector: 'episode-modal',
   templateUrl: './episodeDetailDialogComponent.component.html',
-  exportAs: 'child'
+  exportAs: 'child',
+  providers:[TorrentService]
 })
 export class episodeDetailDialogComponent {
   @ViewChild('lgModal') lgModal;
   @Input() episode;
   @Input() season;
+  @Input() TVShow;
+  constructor(private torrentService: TorrentService){
+
+  }
   show(){
     this.lgModal.show();
+    this.torrentService.get_Torrents(this.TVShow,this.season.season_number, this.episode.episode_number).subscribe(
+        data => {
+            console.log(data);
+        }
+    );
   }
 }
