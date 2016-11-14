580021979cdd09e96e12d940326e54a03f6a6be3
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-11-14 17:33:33 -0400

    #10 a la componente del detalle del episodio, se agrega lodash para organizar los enlaces que se encontraron, son agrupados por el grupo release.

--- source/client/app/body/detail/episodeDetailDialogComponent.component.ts
+++ source/client/app/body/detail/episodeDetailDialogComponent.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, Input }  from '@angular/core';
 import { TorrentService }               from './torrent.service';
+import * as _                           from 'lodash';
 @Component({
     moduleId    : module.id, 
   selector: 'episode-modal',
@@ -12,6 +13,7 @@ export class episodeDetailDialogComponent {
   @Input() episode;
   @Input() season;
   @Input() TVShow;
+  private links;
   constructor(private torrentService: TorrentService){
 
   }
@@ -19,7 +21,58 @@ export class episodeDetailDialogComponent {
     this.lgModal.show();
     this.torrentService.get_Torrents(this.TVShow,this.season.season_number, this.episode.episode_number).subscribe(
         data => {
-            console.log(data);
+            //console.log(data);
+            //let result = _.groupBy(data.result,"group");
+            let links = [];
+            //console.log(data.result);
+            _.forEachRight(data.result, (element, index)=>{
+                if(links.length===0){
+                  links.push({
+                    group:element.group,
+                    data:[
+                      {
+                      title:element.title,
+                      description:element.description,
+                      href:element.href,
+                      group:element.group,
+                      info_torrent:element.info_torrent,
+                      link:element.link,
+                      torrent:element.torrent
+                      }
+                    ]
+                  });
+                }else{
+                  let pos = _.findIndex(links, {'group':element.group});
+                  if(pos>-1){
+                    links[pos].data.push({
+                      title:element.title,
+                      description:element.description,
+                      href:element.href,
+                      group:element.group,
+                      info_torrent:element.info_torrent,
+                      link:element.link,
+                      torrent:element.torrent
+                    });
+                  }else{
+                    links.push({
+                    group:element.group,
+                    data:[
+                      {
+                      title:element.title,
+                      description:element.description,
+                      href:element.href,
+                      group:element.group,
+                      info_torrent:element.info_torrent,
+                      link:element.link,
+                      torrent:element.torrent
+                      }
+                    ]
+                  });
+                  }
+                }
+            });
+            this.links=links;
+            //console.log(links);
         }
     );
   }
