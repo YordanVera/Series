03ecbd2092d4be8cb15b1667ff010a931f4638ef
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-11-14 20:06:56 -0400

    #10 se agregan variables de control para la carga de datos y error

--- source/client/app/body/detail/episodeDetailDialogComponent.component.ts
+++ source/client/app/body/detail/episodeDetailDialogComponent.component.ts
@@ -14,65 +14,71 @@ export class episodeDetailDialogComponent {
   @Input() season;
   @Input() TVShow;
   private links;
+  private error: boolean;
+  private _isLoading: boolean;
   constructor(private torrentService: TorrentService){
-
+    this.error=false;
+    this._isLoading=true;
   }
   show(){
     this.lgModal.show();
     this.torrentService.get_Torrents(this.TVShow,this.season.season_number, this.episode.episode_number).subscribe(
         data => {
-            //console.log(data);
-            //let result = _.groupBy(data.result,"group");
             let links = [];
-            //console.log(data.result);
-            _.forEachRight(data.result, (element, index)=>{
-                if(links.length===0){
-                  links.push({
-                    group:element.group,
-                    data:[
-                      {
-                      title:element.title,
-                      description:element.description,
-                      href:element.href,
-                      group:element.group,
-                      info_torrent:element.info_torrent,
-                      link:element.link,
-                      torrent:element.torrent
+            if(data.success){
+                _.forEachRight(data.result, (element, index)=>{
+                    if(links.length===0){
+                      links.push({
+                        group:element.group,
+                        data:[
+                          {
+                          title:element.title,
+                          description:element.description,
+                          href:element.href,
+                          group:element.group,
+                          info_torrent:element.info_torrent,
+                          link:element.link,
+                          torrent:element.torrent
+                          }
+                        ]
+                      });
+                    }else{
+                      let pos = _.findIndex(links, {'group':element.group});
+                      if(pos>-1){
+                        links[pos].data.push({
+                          title:element.title,
+                          description:element.description,
+                          href:element.href,
+                          group:element.group,
+                          info_torrent:element.info_torrent,
+                          link:element.link,
+                          torrent:element.torrent
+                        });
+                      }else{
+                        links.push({
+                        group:element.group,
+                        data:[
+                          {
+                          title:element.title,
+                          description:element.description,
+                          href:element.href,
+                          group:element.group,
+                          info_torrent:element.info_torrent,
+                          link:element.link,
+                          torrent:element.torrent
+                          }
+                        ]
+                      });
                       }
-                    ]
-                  });
-                }else{
-                  let pos = _.findIndex(links, {'group':element.group});
-                  if(pos>-1){
-                    links[pos].data.push({
-                      title:element.title,
-                      description:element.description,
-                      href:element.href,
-                      group:element.group,
-                      info_torrent:element.info_torrent,
-                      link:element.link,
-                      torrent:element.torrent
-                    });
-                  }else{
-                    links.push({
-                    group:element.group,
-                    data:[
-                      {
-                      title:element.title,
-                      description:element.description,
-                      href:element.href,
-                      group:element.group,
-                      info_torrent:element.info_torrent,
-                      link:element.link,
-                      torrent:element.torrent
-                      }
-                    ]
-                  });
-                  }
-                }
-            });
+                    }
+                });
+                this.error=false;
+                this._isLoading=false;
+            }else{
+              this.error=true;
+              this._isLoading=false;
+            }
             this.links=links;
-            //console.log(links);
         }
     );
   }
