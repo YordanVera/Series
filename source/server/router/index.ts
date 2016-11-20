38eb916c1683a45b96ec9850431c105fb414f792
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-11-19 22:49:40 -0400

    #12 se eliminan las clases torrent_routes y subs_routes, quien asume ambas partes es links_routes. * links_routes obtiene los links de los torrents, además de la información de leechers/seeders, posteriormente obtiene los subtítulos * como google bloquea muchas búsquedas, se agrego un settimout con 1 segundo como medida parche para el scraping de google.

--- source/server/router/index.ts
+++ source/server/router/index.ts
@@ -1,12 +1,10 @@
 import * as express         from 'express';
 import { tmdb_services }    from '../tmdb/tmdb_services';
 import { tvshow_routes }    from './routes/tvshow';
-import { torrent_routes }   from './routes/torrent';
-import { subs_routes }      from './routes/subs';
+import { links_routes }      from './routes/links';
 export class RouterETV {
     constructor(app, tmdb_services){
         new tvshow_routes(app, tmdb_services);
-        new torrent_routes(app);
-        new subs_routes(app);
+        new links_routes(app);
     }
 }
