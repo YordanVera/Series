fe645bfcad9d5dcc122de508bb000146abbda47c
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-11-21 02:30:16 -0400

    #13 se actualiza la componente detail + se agrega el servicio de status + se agrega lodash + se agrega nueva funcionalidad a selectSeason, una vez que se obtiene la información de la temporada, se obtienen los estados de la temporada correspondiente, luego se asignan a los episodios + se agrega nueva función changeStatus, que modifica el estado de un episodio descargado/visto, o si no posee un estado se lo crea.

--- source/client/app/body/detail/detail.component.ts
+++ source/client/app/body/detail/detail.component.ts
@@ -2,13 +2,15 @@ import { Component }                from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
 import { CoverService }             from '../cover/cover.service';
+import { StatusService }            from './status.service';
 import { TVShow }                   from '../cover/tvshow';
+import * as _                       from 'lodash';
 
 @Component({
     moduleId    : module.id, 
     selector    : 'detail',
     templateUrl : './detail.component.html',
-    providers   : [CoverService]
+    providers   : [CoverService, StatusService]
 })
 export class DetailComponent {
     private TVShow  : TVShow;
@@ -18,7 +20,8 @@ export class DetailComponent {
     private _isSeasonSelected : boolean;
     constructor(private route           : ActivatedRoute,
                 private location        : Location,
-                private coverService    : CoverService){
+                private coverService    : CoverService,
+                private statusService   : StatusService){
                     this.TVShow = new TVShow;
                     this._isLoading = this._isLoadingDetail = true;
                     this._isSeasonSelected = false; 
@@ -54,7 +57,50 @@ export class DetailComponent {
             data => {
                 this.season_selected=data.result;
                 this._isSeasonSelected=true;
+                this.statusService.get_status(this.TVShow.id_tvshow, this.season_selected.season_number).subscribe(
+                    _result => {
+                        if(_result.success && _result.result.length>0){
+                            let check = _result.result;
+                            _.forEach(check, (element, index)=>{
+                                let idx = _.findIndex(this.season_selected.episodes, {episode_number:element.episode});
+                                if(idx>-1){
+                                    this.season_selected.episodes[idx]['id_status'] = element.id_status;
+                                    element.downloaded === 1 ? this.season_selected.episodes[idx]['downloaded'] = true : this.season_selected.episodes[idx]['downloaded'] = false;
+                                    element.viewed === 1 ? this.season_selected.episodes[idx]['viewed'] = true : this.season_selected.episodes[idx]['viewed'] = false;
+                                }
+                            });
+                        }
+                    }
+                );
             }
         );
+    }
+    changeStatus(op, event, cap, id_tvshow){
+        op === 1 ? cap.downloaded = event.checked: cap.downloaded=cap.downloaded;
+        op === 2 ? cap.viewed = event.checked: cap.viewed=cap.viewed;
+        if(cap.id_status>=0){
+            this.statusService.update_status({
+                downloaded  :cap.downloaded, 
+                viewed      :cap.viewed,
+                id_status   :cap.id_status
+            }).subscribe(
+                data => {
+                }
+            );
+        }else{
+            this.statusService.add_status({
+                id_tvshow   :id_tvshow,
+                downloaded  :cap.downloaded, 
+                viewed      :cap.viewed,
+                season      :cap.season_number,
+                episode     :cap.episode_number
+            }).subscribe(
+                data =>{
+                    if(data.success){
+                        cap.id_status = data.id_status;
+                    }
+                }
+            );
+        }
     }
 }
