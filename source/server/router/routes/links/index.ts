6bb7f7ff77ff3f1c98adb15a026ed81c5c3e6f54
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-11-24 04:38:22 -0400

    #23 +se agrega subdivx a la clase controladora de los links + se concatena la búsqueda de los subtítulos una vez obtenidos los links de torrent (extratorren y thepiratebay). por ahora solo muestra el mensaje retornado en subdivx.search(); + se agrega método groups_realse, que se encarga de obtener los grupos de cada link, para eso se importa group_list + se agrega método get_resolution, que se encarga de obtener la resolución por cada link (720/1080)

--- source/server/router/routes/links/index.ts
+++ source/server/router/routes/links/index.ts
@@ -5,16 +5,20 @@ import * as cheerio         from 'cheerio';
 import * as Rx              from 'rxjs/Rx';
 import { extratorrent }     from './extratorrent';
 import { thepiratebay }     from './thepiratebay'; 
+import { subdivx }          from './subdivx';
+import { _group_list }      from './groups';
 
 export class links_routes {
     private _app            : express.Express;
     private extratorrent    : extratorrent;
     private thepiratebay    : thepiratebay;
+    private subdivx         : subdivx;
 
     constructor(app){
         this._app           = app;
         this.extratorrent   = new extratorrent();
         this.thepiratebay   = new thepiratebay();
+        this.subdivx        = new subdivx();
         this.setRoutes();
     }
     public setRoutes(){
@@ -40,15 +44,54 @@ export class links_routes {
                         //llamada a búsqueda en thepiratebay
                         return this.thepiratebay.search(TVShow_name, season, episode);
                     })
+                    .flatMap(data => {
+                        //se obtiene grupo realease por cada link
+                        _result = this.groups_release(_result);
+                        //se obtienen las resoluciones por cada link
+                        _result = this.get_resolution(_result);
+                        //se ordena
+                        _result = _.orderBy(_result, ['group'], ['asc']);
+                        //se genera la búsqueda de subtítulos, pasando el conjunto de links
+                        return this.subdivx.search(_result, TVShow_name, season, episode);
+                    })
                     .subscribe(data => {
-                        //resultado de thepiratebay se añade a los de extratorrent
-                        _.forEach(data, (e)=>{
-                            _result.push(e);
-                        });
-                        console.log(_result);
+                        //resultado de subdivx
+                        console.log(data);
                         return res.json({success: true});
                 });
             }
         });
+    }
+    protected groups_release(links){
+        let _links = links;
+        _.forEach(_links, (element, index)=>{
+            _.forEach(_group_list, (group)=>{
+                let name : string = _.lowerCase(element.name);
+                let grupo : string = group;
+                if(name.indexOf(grupo) !== -1){
+                    _links[index]["group"] = grupo;
+                }
+            });
+        });
+        return _links;
+    }
+    protected get_resolution(links){
+        let _links = [];
+        _.forEach(links, (element, index)=>{
+            if(element.name.indexOf('1080')!==-1){
+                let e = element;
+                e['res'] = '1080';
+                _links.push(e);
+            }else{
+                if(element.name.indexOf('720')!==-1){
+                    let e = element;
+                    e['res'] = '720';
+                    _links.push(e);
+                }else{
+                    _links.push(element);
+                }
+            }
+        });
+        return _links;
     }
 }
