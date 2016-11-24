d06f57e4a2c261cc2e09db5298c27156fbfe419d
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-11-24 03:57:19 -0400

    #19 + se crea clase piratebay

---
+++ source/server/router/routes/links/thepiratebay.ts
@@ -0,0 +1,54 @@
+import * as _           from 'lodash';
+import * as request     from 'request';
+import * as cheerio     from 'cheerio';
+import * as Rx          from 'rxjs/Rx';
+import { _group_list }  from './groups';
+var PirateBay = require('thepiratebay');
+
+export class thepiratebay {
+    constructor(){
+
+    }
+    public search(TVShow_name: string, season: number, episode: number){
+        let subject = new Rx.Subject();
+        let query = this.create_query(TVShow_name, season, episode);
+        PirateBay.search(query, {category: 208})
+                .then(results => {
+                    _.forEach(results, (e)=>{
+                        e['site'] = 'The Pirate Bay';
+                    });
+                    subject.next(results);
+                })
+                .catch(err => console.log(err))
+        return subject;
+    }
+    protected create_query(TVShow_name: string, season: number, episode: number){
+        let cap = '';
+        if(season<=9 && episode<=9){
+            cap = 'S0'+season+'E0'+episode;
+        }else if(season<=9 && episode>9){
+            cap = 'S0'+season+'E'+episode;
+        }
+        else if(season>9 && episode<=9){
+            cap = 'S'+season+'E0'+episode;
+        }
+        else if(season>9 && episode>9){
+            cap = 'S'+season+'E'+episode;
+        }
+        return TVShow_name+' '+cap;
+    }
+    protected delete_duplicate(links){
+        let _links = [];
+        _.forEach(links, (element, index)=>{
+            if(_links.length<1){
+                _links.push(element);
+            }else{
+                let idx_element = _.findIndex(_links,{id:element.id});
+                if(idx_element===-1){
+                    _links.push(element);
+                }
+            }
+        });
+        return _links;
+    }
+};
