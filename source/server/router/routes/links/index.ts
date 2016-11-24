fa76ec75e901f8b5279ff8c06f52eecfa8d2a7de
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-11-24 04:06:39 -0400

    #19 + se agrega clase 'thepiratebay' para añadir su búsqueda. + se crea una variable _result que contendrá los resultados de 'thepiratebay' y 'extratorrent'. + se 'concatenan' ambas llamadas, primero se realiza la búsqueda en extratorrent, luego se busca en thepiratebay, finalmente se impreme en consola el resultado.

--- source/server/router/routes/links/index.ts
+++ source/server/router/routes/links/index.ts
@@ -4,14 +4,17 @@ import * as request         from 'request';
 import * as cheerio         from 'cheerio';
 import * as Rx              from 'rxjs/Rx';
 import { extratorrent }     from './extratorrent';
+import { thepiratebay }     from './thepiratebay'; 
 
 export class links_routes {
     private _app            : express.Express;
     private extratorrent    : extratorrent;
+    private thepiratebay    : thepiratebay;
 
     constructor(app){
         this._app           = app;
         this.extratorrent   = new extratorrent();
+        this.thepiratebay   = new thepiratebay();
         this.setRoutes();
     }
     public setRoutes(){
@@ -25,13 +28,26 @@ export class links_routes {
                 let TVShow_name = req.params.TVShow_name;
                 let season = req.params.season;
                 let episode = req.params.episode;
+                var _result = [];
                 //search in extratorrent
                 this.extratorrent
                     .search(TVShow_name, season, episode)
+                    .flatMap(data => {
+                        //resultado de extratorrent se almacena en _result
+                        _.forEach(data, (e)=>{
+                            _result.push(e);
+                        });
+                        //llamada a búsqueda en thepiratebay
+                        return this.thepiratebay.search(TVShow_name, season, episode);
+                    })
                     .subscribe(data => {
-                        console.log(data);
+                        //resultado de thepiratebay se añade a los de extratorrent
+                        _.forEach(data, (e)=>{
+                            _result.push(e);
+                        });
+                        console.log(_result);
                         return res.json({success: true});
-                    });
+                });
             }
         });
     }
