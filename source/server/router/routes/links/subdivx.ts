24f84a62f1aff79c6804a33f2ccbce0af9340ac1
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-11-24 20:19:44 -0400

    #23 +actualización menor a nombre de método create_query -> create_url +se realiza la búsqueda de los datos obtenidos de la consulta a la web de subdivx. +por cada dato encontrado se verifican tres casos, del primero se obtiene el link del subtitulo, el segundo la descripción y finalmente el tercero el enlace de descarga.

--- source/server/router/routes/links/subdivx.ts
+++ source/server/router/routes/links/subdivx.ts
@@ -7,16 +7,69 @@ export class subdivx {
     constructor(){}
     public search(links, TVShow_name: string, season: number, episode: number){
         let subject = new Rx.Subject();
-        let query = this.create_query(TVShow_name, season, episode);
-        request(query, (error, response, html)=>{
+        let url = this.create_url(TVShow_name, season, episode);
+        request(url, (error, response, html)=>{
             if(!error){
                 var $ = cheerio.load(html, {normalizeWhitespace: false, xmlMode: false, decodeEntities: true});
-                subject.next('búsqueda en subdivx');
+                var _self = this;
+                var contenedor_izq = [];
+                $('#contenedor_izq')
+                .children('div')
+                .filter(function(idx, element){
+                    _.forEach(element.children, (element, index)=>{
+                        if(element.name === 'div' && element.type === 'tag'){
+                            contenedor_izq.push(element);
+                        }
+                    });
+                    return true;
+                });
+                var _case = 0;
+                var list_subtitles = [];
+                var _subtitle = null;
+                _.forEach(contenedor_izq, (element, index)=>{
+                    switch (_case){
+                        //inicia el subtitulo, y obtiene el link del subtitulo
+                        case 0:{
+                            _subtitle = {
+                                href : null,
+                                desc : null,
+                                link : null
+                            };
+                            if(element.attribs.id === 'menu_titulo_buscador'){
+                                if(element.children[0].attribs.class === 'titulo_menu_izq'){
+                                    _subtitle.href = element.children[0].attribs.href;
+                                    _case++;
+                                }
+                            }
+                        }
+                        //obtiene la descripción del subtitulo
+                        case 1:{ 
+                            if(element.attribs.id === 'buscador_detalle_sub'){
+                                if(element.children[0].type==='text'){
+                                    _subtitle.desc=element.children[0].data;
+                                    _case++;
+                                }
+                            }
+                        }
+                        //obtiene el link de descarga del subtitulo y reinicia el _case
+                        case 2:{
+                            if(element.attribs.id === 'buscador_detalle_sub_datos'){
+                                let last_child = element.children[element.children.length-1];
+                                if(last_child.type === 'tag' && last_child.name === 'a'){
+                                    _subtitle.link=last_child.attribs.href;
+                                    _case=0;
+                                    list_subtitles.push(_subtitle);
+                                }
+                            }
+                        }
+                    }
+                });
+                subject.next(list_subtitles);
             }
         });
         return subject;
     }
-    protected create_query(TVShow_name: string, season: number, episode: number){
+    protected create_url(TVShow_name: string, season: number, episode: number){
         let cap = '';
         if(season<=9 && episode<=9){
             cap = 'S0'+season+'E0'+episode;
