be9daf666c5f6299c0327943f35b75b58b4bec41
yordan_vera_castillo <yordanveracastillo@gmail.com>
2016-11-26 15:01:03 -0400

    #23  + se agrega el procedimiento para enlazar los links y los subtítulos. + se agrega procedimiento que da formato a los links. + se agrega instrucciones de control en caso de que no se encuentren links, enviando posteriormente un mensaje de error al cliente.

--- source/server/router/routes/links/index.ts
+++ source/server/router/routes/links/index.ts
@@ -34,31 +34,46 @@ export class links_routes {
                 let episode = req.params.episode;
                 var _result = [];
                 //search in extratorrent
-                this.extratorrent
-                    .search(TVShow_name, season, episode)
-                    .flatMap(data => {
-                        //resultado de extratorrent se almacena en _result
-                        _.forEach(data, (e)=>{
-                            _result.push(e);
-                        });
-                        //llamada a búsqueda en thepiratebay
-                        return this.thepiratebay.search(TVShow_name, season, episode);
-                    })
-                    .flatMap(data => {
-                        //se obtiene grupo realease por cada link
-                        _result = this.groups_release(_result);
-                        //se obtienen las resoluciones por cada link
-                        _result = this.get_resolution(_result);
-                        //se ordena
-                        _result = _.orderBy(_result, ['group'], ['asc']);
-                        //se genera la búsqueda de subtítulos, pasando el conjunto de links
-                        return this.subdivx.search(_result, TVShow_name, season, episode);
-                    })
-                    .subscribe(data => {
-                        //resultado de subdivx
-                        console.log(data);
-                        return res.json({success: true});
-                });
+                this.extratorrent.search(TVShow_name, season, episode)
+                    .flatMap(
+                        data => {
+                            //resultado de extratorrent se almacena en _result
+                            if(data !== 'no data'){
+                                _.forEach(data, (e)=>{
+                                    _result.push(e);
+                                });
+                            }
+                            //llamada a búsqueda en thepiratebay
+                            return this.thepiratebay.search(TVShow_name, season, episode);
+                        })
+                    .flatMap(
+                        data => {
+                            if(data !== 'no data'){
+                                _.forEach(data, (e)=>{
+                                    _result.push(e);
+                                });
+                            }
+                            //se obtiene grupo realease por cada link
+                            _result = this.groups_release(_result);
+                            //se obtienen las resoluciones por cada link 
+                            _result = this.get_resolution(_result);
+                            //se genera la búsqueda de subtítulos, pasando el conjunto de links
+                            return this.subdivx.search(_result, TVShow_name, season, episode);
+                        })
+                    .subscribe(data => {                    
+                        if(_result.length>0){
+                            _result = this.format_links(_result);
+                            _result = _.orderBy(_result, ['group'], ['asc']);
+                            //realizar matching entre enlaces y subtítulos
+                            let final_result = this.match_links_subs(_result, data);
+                            return res.json({success: true, result: final_result});
+                        }else{
+                            return res.json({success: false, error: 'no data'});
+                        }
+
+                    }, error => {
+                        return res.json({success: false, error: error});
+                    });
             }
         });
     }
@@ -93,5 +108,106 @@ export class links_routes {
             }
         });
         return _links;
+    }
+    protected format_links(links){
+        let _links = [];
+        _.forEachRight(links, (element, index)=>{
+                    if(_links.length===0){
+                      _links.push({
+                        group:element.group,
+                        data:[
+                            element
+                        ]
+                      });
+
+                    }else{
+                      let pos = _.findIndex(_links, {'group':element.group});
+                      if(pos>-1){
+                        _links[pos].data.push(element);
+                      }else{
+                        _links.push({
+                        group:element.group,
+                        data:[
+                          element
+                        ]
+                      });
+                      }
+                    }
+                });
+        return _links;
+    }
+    protected match_links_subs(list_links, list_subs){
+        let links = list_links;
+        _.forEach(links, (element, index)=>{
+            element['subs']=[];
+            _.forEach(element.data, (elm, idx)=>{
+                if(elm.res === '1080' && typeof elm.group !== 'undefined'){
+                    _.forEach(list_subs, (e,i)=>{
+                        let idx_group = _.findIndex(e.groups, (group)=>{
+                            return group === element.group;
+                        });
+                        if(e.res_1080 && idx_group>-1){
+                            let idx_sub = _.findIndex(element.subs, {link:e.link});
+                            if(idx_sub<0){
+                                element.subs.push(e);
+                            }
+                        }
+                    });
+                }else if(elm.res === '1080' && typeof elm.group === 'undefined'){
+                    _.forEach(list_subs, (e,i)=>{
+                        if(e.res_1080 && e.groups.length === 0){
+                            let idx_sub = _.findIndex(element.subs, {link:e.link});
+                            if(idx_sub<0){
+                                element.subs.push(e);
+                            }
+                        }
+                    });
+                }else if(elm.res === '720' && typeof elm.group !== 'undefined'){
+                    _.forEach(list_subs, (e,i)=>{
+                        let idx_group = _.findIndex(e.groups, (group)=>{
+                            return group === element.group;
+                        });
+                        if(e.res_720 && idx_group>-1){
+                            let idx_sub = _.findIndex(element.subs, {link:e.link});
+                            if(idx_sub<0){
+                                element.subs.push(e);
+                            }
+                        }
+                    });
+                }else if(elm.res === '720' && typeof elm.group === 'undefined'){
+                    _.forEach(list_subs, (e,i)=>{
+                        if(e.res_720 && e.groups.length === 0){
+                            let idx_sub = _.findIndex(element.subs, {link:e.link});
+                            if(idx_sub<0){
+                                element.subs.push(e);
+                            }
+                        }
+                    });
+                }else if(typeof elm.res === 'undefined' && typeof elm.group !== 'undefined'){
+                    _.forEach(list_subs, (e,i)=>{
+                        let idx_group = _.findIndex(e.groups, (group)=>{
+                            return group === element.group;
+                        });
+                        if(idx_group>-1){
+                            let idx_sub = _.findIndex(element.subs, {link:e.link});
+                            if(idx_sub<0){
+                                element.subs.push(e);
+                            }
+                        }
+                    });
+                }else if(typeof elm.res === 'undefined' && typeof elm.group === 'undefined'){
+                    _.forEach(list_subs, (e,i)=>{
+                        if(e.groups.length===0){
+                            let idx_sub = _.findIndex(element.subs, {link:e.link});
+                            if(idx_sub<0){
+                                element.subs.push(e);
+                            }
+                        }
+                    });
+                }
+            });
+
+        });
+        return links;
     }
 }
